d5d30aeeab46a23a4a643a21d319ec30d75078d0
jayzone91 <register@j4yz0n3.de>
2024-05-22 08:11:09 +0200

    Create oil.lua

---
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,38 @@
+return {
+  "stevearc/oil.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("oil").setup({
+      columns = { "icons" },
+      keymaps = {
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = "actions.select_vsplit",
+        ["<C-h>"] = "actions.select_split",
+        ["<C-t>"] = "actions.select_tab",
+        ["<C-p>"] = "actions.preview",
+        ["<Esc>"] = "actions.close",
+        ["<C-l>"] = "actions.refresh",
+        ["-"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["~"] = "actions.tcd",
+        ["gs"] = "actions.change_sort",
+        ["gx"] = "actions.open_external",
+        ["g."] = "actions.toggle_hidden",
+        ["g\\"] = "actions.toggle_trash",
+      },
+      view_options = {
+        show_hidden = true,
+      },
+    })
+    vim.keymap.set(
+      "n",
+      "<leader>d",
+      require("oil").toggle_float,
+      { desc = "Open Directory in Oil" }
+    )
+  end,
+}
