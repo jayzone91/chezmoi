6030a8ee4df4f16c37470fc18708fe57d0803222
jayzone91 <register@j4yz0n3.de>
2024-05-30 06:35:29 +0200

    diesmal richtiger ort für wezterm

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,10 +1,10 @@
--- Pull in the Wezterm API
+-- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This will hold the configuration
+-- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply yout config
+-- This is where you actually apply your config choices
 
 -- and finally, return the configuration to wezterm
 return config
