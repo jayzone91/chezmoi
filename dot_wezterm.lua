9bd5948fa773cd8989e9e3f18b7c486620bbcd58
jayzone91 <register@j4yz0n3.de>
2024-05-30 06:40:57 +0200

    lazyvim weg, weil es sich ständig ändert. wezterm config

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -6,5 +6,10 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
+-- Set Theme
+config.color_scheme = "rose-pine-moon"
+-- Set your Font
+config.font = wezterm.font("MesloLGS NF")
+
 -- and finally, return the configuration to wezterm
 return config
