07ded7cd28bf58d9d08b4633f6f428f0b0c3cb28
jayzone91 <register@j4yz0n3.de>
2024-06-22 14:27:39 +0200

    fish config added

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+fzf --fish | source
+zoxide init fish | source
+starship init fish | source
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
