33e994e00aeee4cba1afb7527fb6b8905b4f86d5
jayzone91 <register@j4yz0n3.de>
2024-07-03 20:10:28 +0200

    komplett neues nvim / kein bock mehr, einfach lazyvim mit ein bisschen custom kram

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,33 +1,14 @@
--- Set leader key
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
 
--- Vim Mappings (Plugin specific mappings are in the plugin specs)
-
---- Sets Keymaps
 ---@param mode string|table
----@param key string
----@param func string|function
----@param opts table|nil
-local map = function(mode, key, func, opts)
-  vim.keymap.set(mode, key, func, opts or {})
+---@param keys string
+---@param fun string|function
+---@param opts table?
+local map = function(mode, keys, fun, opts)
+  vim.keymap.set(mode, keys, fun, opts)
 end
-
--- save file with CTRL-S
-map(
-  { "i", "x", "n", "s", "v" },
-  "<c-s>",
-  "<cmd>w<cr><esc>",
-  { desc = "Save File" }
-)
-
--- Leave Insert mode with CTRL-c
-map("i", "<c-c>", "<esc>")
-
--- Increment and decrement
--- Disabled, instead we use dial.nvim
--- map("n", "+", "<c-a>")
--- map("n", "-", "<c-x>")
 
 -- better up/down
 map(
@@ -80,45 +61,10 @@ map("n", "<c-right>", "<C-w>l", { remap = true })
 -- Clear search with <esc>
 map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>")
 
--- Exit vim
-map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit Neovim" })
-
 -- split Window
 map("n", "<leader>ss", "<C-w>s", { desc = "Split Screen horizontal" })
 map("n", "<leader>sv", "<C-w>v", { desc = "Split Screen vertical" })
 map("n", "<leader>se", "<C-w>=", { desc = "Make Split equal size" })
 
--- tabs
-map("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new Tab" })
-map("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current Tab" })
-map("n", "<Tab>", "<cmd>tabn<CR>", { desc = "Go to next Tab" })
-map("n", "<S-Tab>", "<cmd>tabp<CR>", { desc = "Go to prev Tab" })
-map(
-  "n",
-  "<leader>tf",
-  "<cmd>tabnew %<CR>",
-  { desc = "Open current Buffer in new tab" }
-)
-
 -- Select All
 map({ "i", "n" }, "<C-a>", "<esc>gg<S-v>G")
-
--- Terminal Stuff
-map("n", "<leader>ts", function()
-  vim.cmd("belowright 12split")
-  vim.cmd("set winfixheight")
-  vim.cmd("term")
-  vim.cmd("startinsert")
-end, { desc = "Split Screen and open Terminal on the lower part." })
-
-map("n", "<leader>tn", function()
-  vim.cmd("tabnew")
-  vim.cmd("terminal")
-  vim.cmd("startinsert")
-end, { desc = "Open Terminal in a new Tab" })
-
-map("n", "<leader>tv", function()
-  vim.cmd("vsplit")
-  vim.cmd("terminal")
-  vim.cmd("startinsert")
-end, { desc = "Split Screen vertical and open Terminal on the left part." })
